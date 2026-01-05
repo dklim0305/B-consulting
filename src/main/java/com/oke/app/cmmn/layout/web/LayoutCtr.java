18697db0870587ed31fda251f8ca77fc80e93ffa
dbsrkdud <yungayoung63@gmail.com>
2026-01-05 10:26:14 +0900

    스프링 웹 세팅

---
+++ src/main/java/com/oke/app/cmmn/layout/web/LayoutCtr.java
@@ -0,0 +1,15 @@
+package com.oke.app.cmmn.layout.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LayoutCtr {
+
+
+    @RequestMapping("/layout/cmmnlib.do")
+    public String cmmnlib() {
+        return "cmmn/cmmnLib";
+    }
+
+}
